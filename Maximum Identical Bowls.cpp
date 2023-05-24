ca2feb4cdca6dee20987d0a35eabd96057a65570
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-05-24 19:56:27 +0530

    Create Maximum Identical Bowls.cpp

---
+++ Maximum Identical Bowls.cpp
@@ -0,0 +1,16 @@
+int getMaximum(int N, vector<int> &arr) {
+        long long int sum=0;
+        for (int i=0;i<N;i++)
+        {
+            sum+=arr[i];
+        }
+        for (int i=N;i>0;i--)
+        {
+            if (sum%i==0)
+            {
+                return i;
+                break;
+            }
+        }
+        // code here
+    }
