d856fa700a3567befaa9272003612fa83e9899cc
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-06-14 03:41:03 +0530

    Create Maximum Diamonds.cpp

---
+++ Maximum Diamonds.cpp
@@ -0,0 +1,14 @@
+long long maxDiamonds(int A[], int N, int K) {
+        long long int ans=0;
+        priority_queue<int> pq;
+        for(int i=0;i<N;i++) pq.push(A[i]);
+        while(K--){
+            int temp=pq.top();
+            ans+=temp;
+            pq.pop();
+            temp/=2;
+            pq.push(temp);
+        }
+        return ans;
+        // code here
+    }
