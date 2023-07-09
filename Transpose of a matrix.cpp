56608d18a693b8d85208bebccb364f5f3f1d58b7
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-07-10 01:48:12 +0530

    Create Transpose of a matrix.cpp

---
+++ Transpose of a matrix.cpp
@@ -0,0 +1,8 @@
+void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++)
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
